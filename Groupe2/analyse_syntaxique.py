1a67711b6aae99780ebecc9c045d192b577bd652
Shami THIRION SEN <shami.contact@gmail.com>
2024-05-20 21:28:51 +0200

    ajout complexite en temps

--- Groupe2/analyse_syntaxique.py
+++ Groupe2/analyse_syntaxique.py
@@ -4,12 +4,25 @@ import spacy
 nlp = spacy.load("fr_core_news_sm")
 
 
+import time
+
+def time_wrapper(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Function {func.__name__} took {end_time - start_time} seconds")
+        return result
+    return wrapper
+
+@time_wrapper
 def preprocess_gp1(inputFile):
     with open(inputFile, 'r') as file:
         textes = file.readlines()
         print(nlp.pipeline)
         return textes
 
+@time_wrapper
 def process_gp1(textes):
     res = list(nlp.pipe(textes, disable=["parser", "ner", "lemmatizer", "attribute_ruler"]))
     """
@@ -21,6 +34,7 @@ def process_gp1(textes):
 # On n'enlève pas morphologizer, car c'est le module qui POS tag.
 
 
+@time_wrapper
 def process_gp2(docs):
     res = list(nlp.pipe(docs, disable=["morphologizer", "ner", "lemmatizer", "attribute_ruler"]))
     """
@@ -30,4 +44,4 @@ def process_gp2(docs):
     """
     return res
 
-process_gp2(process_gp1(preprocess_gp1('petit_test.txt')))
+process_gp2(process_gp1(preprocess_gp1('../Groupe1/petit_test.txt')))
