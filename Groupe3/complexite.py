fbf1ba3b878188d2b02bb7ede800a1493b4cfc21
Aliwasan <awallard@yahoo.fr>
2024-05-21 22:13:51 +0200

    tjs_en_attente

--- Groupe3/complexite.py
+++ Groupe3/complexite.py
@@ -5,6 +5,7 @@
 Il y a deux manières de résoudre ce problème:
 - augmenter la limite de recursion avec le module sys (à vos risques et périls...)
 - transformer le code pour passer d'un code récursif à un code itératif (ce qui n'est pas le but de ce projet)
+Calcul de complexité avec ajout de 2 paramètres : un pour la complexité en espace et un pour la complexité en temps
 """
 
 import spacy
@@ -14,46 +15,56 @@ import sys
 sys.setrecursionlimit(6000)
 
 
-def preprocess_file(inputFile: str, cpt_analyse_spacy, cpt_process_file, cpt_process_line, cpt_espace) -> list[dict]:
+def preprocess_file(inputFile: str, cpt_analyse_spacy, cpt_process_file, cpt_process_line, cpt_espace, cpt_temps) -> tuple[list[dict], int, int]:
 	""" ouvre le fichier texte, le découpe au niveau des '\n'
 	et appelle la fonction analyse_spacy() pour analyser le texte
 	Args:
 		inputFile (str): chemin vers le fichier
 	Returns:
-		list[dict]: chaque dico correspond à une phrase analysé
+		1 dico = 1 SpacyDoc = 1 phrase analysée
+		tuple : (list[], cpt_espace, cpt_temps) ou tuple (list[dict[i:(tok,label)]], cpt_espace, cpt_temps)
 	"""
 	with open(inputFile, 'r') as file:
 		texte = [line.rstrip() for line in file.readlines()] # list[str]
 		if len(texte) == 0:
-			return []
-		return analyse_spacy(texte, cpt_analyse_spacy, cpt_process_file, cpt_process_line, cpt_espace)
+			return [], cpt_espace, cpt_temps
+			cpt_temps += 1
+		cpt_temps += 1
+		print(f'cpt_temps dans preprocess_file : {cpt_temps}')
+		print(f' cpt_espace dans preprocess_file : {cpt_espace}')
+		return analyse_spacy(texte, cpt_analyse_spacy, cpt_process_file, cpt_process_line, cpt_espace, cpt_temps)
 
 
-def analyse_spacy(texte: list[str], cpt_analyse_spacy, cpt_process_file, cpt_process_line, cpt_espace) -> list[dict]:
-	""" on analyse le texte avec spacy : création d'un objet SpacyDoc par ligne/phrase du texte original
+def analyse_spacy(texte: list[str], cpt_analyse_spacy, cpt_process_file, cpt_process_line, cpt_espace, cpt_temps) -> tuple[list[dict], int, int]:
+	""" on analyse le texte avec spacy : création d'un objet SpacyDoc par ligne (donc phrase) du texte original
 	Args:
 		texte (list[str]): liste de phrases
 	Returns:
-		list[dict]: chaque dico correspond à une phrase analysée
+		1 dico = 1 SpacyDoc = 1 phrase analysée
+		tuple (list[dict[i:(tok,label)]] , cpt_espace, cpt_temps)
 	"""
 	docs = list(nlp.pipe(texte, disable=["parser", "morphologizer", "lemmatizer", "attribute_ruler"])) # list[SpacyDoc]
 	cpt_analyse_spacy += len(docs)
-	print(f'cpt_analyse_spacy : {cpt_analyse_spacy}')
+	print(f'cpt_analyse_spacy 	ajout len(docs): {cpt_analyse_spacy}')
 	print(f'cpt_espace : {cpt_espace}')
 	if cpt_analyse_spacy > cpt_espace:
 		cpt_espace += cpt_analyse_spacy
 		print(f'cpt_espace : {cpt_espace}')
 		print(f'cpt_analyse_spacy > cpt_espace : {cpt_espace}')
-	return process_file([], docs, cpt_process_file, cpt_process_line, cpt_espace)
+	cpt_temps += 1
+	print(f'cpt_temps dans analyse_spacy : {cpt_temps}')
+	return process_file([], docs, cpt_process_file, cpt_process_line, cpt_espace, cpt_temps)
 
 
-def process_file(dicos: list[dict], docs: list[spacy.tokens.doc.Doc], cpt_process_file, cpt_process_line, cpt_espace) -> list[dict]:
-	""" traite le texte ligne par ligne jusqu'à renvoyer
+def process_file(dicos: list[dict], docs: list[spacy.tokens.doc.Doc], cpt_process_file, cpt_process_line, cpt_espace, cpt_temps) -> list[dict]:
+	""" 
+	traite les SpacyDocs (donc le texte ligne par ligne) et renvoie les dic[i:(tok,label)]  de la def appelée process_line()
+	i: indice dans la phrase.
 	Args:
-		dicos (list[dict]): chaque dico correspond à une phrase analysé
-		docs (list[spacy.tokens.doc.Doc]): liste de phrases analysées avec Spacy
+		docs (list[spacy.tokens.doc.Doc]): liste de SpacyDocs (de phrases analysées avec Spacy)
 	Returns:
-		list[dict]: chaque dico correspond à une phrase analysée
+		tuple (list[dict[:]] , cpt_espace, cpt_temps) ou 
+		tuple (list[dict[i:(tok,label)]] , cpt_espace, cpt_temps)
 	"""
 	if len(docs) == 0:
 		cpt_process_file += len(dicos)
@@ -61,16 +72,19 @@ def process_file(dicos: list[dict], docs: list[spacy.tokens.doc.Doc], cpt_proces
 		if cpt_process_file > cpt_espace:
 			cpt_espace += cpt_process_file
 			print(f'cpt process_file > cpt_espace : {cpt_espace}')
-		return dicos
+		return dicos, cpt_espace, cpt_temps
 	else:
 		dicos.append({})
-		dicos = process_line(0, dicos, docs[0], cpt_process_line, cpt_espace) # cpt_temps +1 ici aussi
-		# cpt_time +=1
-		return process_file(dicos, docs[1:], cpt_process_file, cpt_process_line, cpt_espace) 
+		cpt_temps +=1
+		dicos, cpt_espace, cpt_temps = process_line(0, dicos, docs[0], cpt_process_line, cpt_espace, cpt_temps	)
+		cpt_temps +=1
+		print(f'cpt_temps dans process_file : {cpt_temps}')
+		print(f'cpt_espace dans process_file : {cpt_espace}')
+		return process_file(dicos, docs[1:], cpt_process_file, cpt_process_line, cpt_espace, cpt_temps) 
 
 
-def process_line(i: int, dicos: list[dict], doc: spacy.tokens.doc.Doc, cpt_process_line, cpt_espace) -> list[dict]:
-	""" traite la ligne analysée par spacy pour créer un dico avec comme clé l'index
+def process_line(i: int, dicos: list[dict], doc: spacy.tokens.doc.Doc, cpt_process_line, cpt_espace, cpt_temps) -> list[dict]:
+	""" traite le SpacyDoc pour remplir un dico avec comme clé l'index
 	du token et comme valeur un tuple (le token, son annotation)
 	Args:
 		i (int): l'index du token à analyser
@@ -88,16 +102,18 @@ def process_line(i: int, dicos: list[dict], doc: spacy.tokens.doc.Doc, cpt_proce
 		if cpt_process_line > cpt_espace:
 			cpt_espace += cpt_process_line
 			print(f'cpt process_line > cpt_espace : {cpt_espace}')
-		return dicos
+		return dicos, cpt_espace, cpt_temps
 	else:
 		if doc[0].ent_iob_ != "O":
 			dicos[-1][i] = (doc[0].text, doc[0].ent_iob_+"-"+doc[0].ent_type_)
 		else:
 			dicos[-1][i] = (doc[0].text, "O")
 		cpt_process_line -= 1 # on décrémente la liste des tokens
-		return process_line(i+1, dicos, doc[1:], cpt_process_line, cpt_espace)
+		cpt_temps +=1
+		print(f'cpt_temps dans process_line : {cpt_temps}')
+		return process_line(i+1, dicos, doc[1:], cpt_process_line, cpt_espace, cpt_temps)
 
 
 if __name__ == "__main__":
-	preprocess_file("petit_test.txt", cpt_analyse_spacy=0, cpt_process_file=0, cpt_process_line=0, cpt_espace=0)
+	print(preprocess_file("petit_test.txt", cpt_analyse_spacy=0, cpt_process_file=0, cpt_process_line=0, cpt_espace=0, cpt_temps=0))
 	#print(preprocess_file("../Corpus/JV-5_semaines_ballon.txt"))
