434ec5ee8ca1e9eb23d2f3f4ce9254c43acb7573
AHMIA Kenza <ahmiakenza@hotmail.com>
2024-05-21 21:46:36 +0200

    ajout de commentaires pour les fonctions

--- Groupe2/groupe2.py
+++ Groupe2/groupe2.py
@@ -18,26 +18,40 @@ n_tokens = []
 n = 0 # nombre de tokens
 
 def time_and_memory_wrapper(func):
+    """
+    Décorateur pour mesurer le temps d'exécution et la mémoire utilisée par une fonction
+    
+    Args:
+        func (callable): La fonction à décorer.
+    
+    Returns:
+        callable: La fonction décorée avec des mesures de temps et de mémoire.
+    """
     def wrapper(*args, **kwargs):
         global tracer_enabled, n, time_data, memory_data, n_tokens # Il est plus prudent de les réinitialiser à ce moment-là Shami
+        # Démarre le traçage de la mémoire si ce n'est pas déjà fait
         if not tracer_enabled:
             tracemalloc.start()
             tracer_enabled = True
-
+        
+        # Mesure de la mémoire et du temps avant et après l'exécution de la fonction    
         snapshot_id0 = tracemalloc.take_snapshot()
         start_time = time.time()
         result = func(*args, **kwargs)
         snapshot_id1 = tracemalloc.take_snapshot()
         end_time = time.time()
 
+        # Calcul et enregistrement des différences de mémoire et de temps, ainsi que du nombre de tokens
         mem_diff = snapshot_id1.compare_to(snapshot_id0, 'lineno')
         time_data.append(end_time - start_time)
         memory_data.append(sum(block.size for block in mem_diff)/10**6)
         n_tokens.append(len(result))
         n += len(result)
 
+        # Affiche les informations de performance pour la fonction
         print(f"Function {func.__name__} took {end_time - start_time} seconds\n and used {sum(block.size for block in mem_diff)/10**6} MB of memory")
 
+        # Arrête le traçage de la mémoire si il était activé
         if tracer_enabled:
             tracemalloc.stop()
             tracer_enabled = False
@@ -46,6 +60,14 @@ def time_and_memory_wrapper(func):
     return wrapper
 
 def plot_complexity():
+    """
+    Fonction pour tracer les données de complexité en temps et en mémoire
+    
+    Utilise les listes globales `n_tokens`, `time_data` et `memory_data` 
+    pour créer un graphique combiné avec deux axes x, y,
+    x : représente le nombre de tokens.
+    y : représente le temps d'exécution.
+    """
     # Tri des trois listes sur le nombre de tokens
     combined = list(zip(n_tokens, time_data, memory_data))
     sorted_on_ntokens = sorted(combined, key=lambda x: x[0])
@@ -80,6 +102,16 @@ def plot_complexity():
 
 @time_and_memory_wrapper
 def process_gp2(docs):
+    """
+    Fonction pour traiter les documents avec le pipeline de traitement NLP de Spacy
+    
+    Args:
+        docs (list): Liste de documents textuels à traiter.
+
+    Returns:
+        list : tok.text, tok.pos_, tok.dep_, tok.head
+
+    """
     res = list(nlp.pipe(docs, disable=["morphologizer", "ner", "lemmatizer", "attribute_ruler"]))
     return res
 
@@ -87,12 +119,14 @@ def process_gp2(docs):
 if __name__ == "__main__":
     input_files = glob.glob('../Corpus/*.txt') # tout les fichiers dans le dossier Corpus
     for input_file in input_files:
+        # Préprocessus et processus de chaque fichier, puis mesure des performances
         process_gp2(process_gp1(preprocess_gp1(input_file)))
         print(f"Processing {input_file}...")
 
     print(f"Total number of tokens: {n}")
     print("Time data:", time_data)
     print("Memory data:", memory_data)
-
+    
+    # Génère et affiche le graphique de complexité
     plot_complexity()
                 
